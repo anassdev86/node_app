13a8d02278ccf6b0436e305ef301b707c9df896d
anassdev86 <blopque20@gmail.com>
2024-06-26 21:59:11 +0100

    add route of api

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
 const PORT = process.env.PORT || 8080;
+const fs = require('fs');
+
+const file_data = fs.readFileSync('./data.json', 'utf-8');
+console.log(file_data);
 
 const server = http.createServer((req, res) => {
+    console.log(req.url);
+    if(req.url === '/api'){
+        res.writeHead(200, {'Content-type': 'application/json'});
+        return res.end(file_data);
+    }
     res.writeHead(200, { 'Content-type': 'text/plain'});
     res.end('hello in first node ');
 });
